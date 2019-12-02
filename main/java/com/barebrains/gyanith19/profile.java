a33153f53a7573b417617f066eda0d04247dfe37
Pushpavel <jpushpavel@gmail.com>
2019-12-02 20:41:20 +0530

    Gyanith-19 repo cloned.

---
+++ main/java/com/barebrains/gyanith19/profile.java
@@ -0,0 +1,27 @@
+package com.barebrains.gyanith19;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        ((Button)findViewById(R.id.backprof)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            getWindow().setStatusBarColor(getResources().getColor(android.R.color.white));
+        }
+    }
+}
